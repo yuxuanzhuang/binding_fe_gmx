e0435f48c7f49de117dc2a093103385b8ad5122a
yuxuan.zhuang@dbb.su.se <yuxuan.zhuang@dbb.su.se>
2023-07-14 14:53:11 +0200

    2d fep

--- tutorials/awh_fep/jobscript.sh
+++ tutorials/awh_fep/jobscript.sh
@@ -27,4 +27,4 @@ module switch gromacs/2023 gromacs=gmx_mpi
 module switch cuda/11.8
 module unload openmpi
 module load openmpi
-srun -n 8 gmx_mpi mdrun -deffnm awh -cpi awh -multidir rep{1..8} -pme gpu -bonded gpu -nb gpu -awh awhinit.xvg -px awh_pullx -pf awh_pullf -maxh 23 -dhdl dhdl
+srun -n 8 gmx_mpi mdrun -deffnm awh -cpi awh -multidir rep{1..8} -awh awhinit.xvg -px awh_pullx -pf awh_pullf -maxh 23 -dhdl dhdl
